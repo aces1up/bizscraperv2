62e5523f1689226c57c6832aa081ee47ba632e81
aces1up <aces1up@gmail.com>
2024-10-04 01:08:12 +0000

    directory restructuring

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to BizScraper in /src/app/page.tsx  </h1>
+    </main>
+  )
+}
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Welcome to BizScraper</h1>
-    </main>
-  )
-}
